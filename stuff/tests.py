36d715113f1f6e7edb0f8839281e432e54502dc2
Matthew Larkin <matthewlarkin8@gmail.com>
2022-08-30 00:54:41 -0400

    Created a class method to setup test data. Added a test for the Stuff model. Test is passing.

--- stuff/tests.py
+++ stuff/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import Stuff
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class StuffTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        testuser = get_user_model().objects.create_user(username="testuser", password="test")
+        testuser.save()
+
+        test_stuff = Stuff.objects.create(name="boat", owner=testuser, description="Water car")
+        test_stuff.save()
+
+    def test_stuff_model(self):
+        stuff = Stuff.objects.get(id=1)
+        actual_owner = str(stuff.owner)
+        actual_name = str(stuff.name)
+        actual_description = str(stuff.description)
+        self.assertEqual(actual_owner, "testuser")
+        self.assertEqual(actual_name, "boat")
+        self.assertEqual(actual_description, "Water car")
